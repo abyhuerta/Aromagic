9a810075f2d881cedcce74c4fd56abc83ed957b6
abyy <128166822+Roxana808@users.noreply.github.com>
2024-10-05 19:29:20 -0500

    set up product.js

--- src/catalog.js
+++ src/catalog.js
@@ -5,7 +5,12 @@ const products = [
     price: 19.99,
     images: [],
     stock: 100,
-    description: "This product is totally not a scam."
+    description: "This product is totally not a scam.",
+    sizes: [],
+    category: "fall"
+}
+{
+    id:2,
 }
 
 ]
