c0285b16ed3ba543446cd2de998cb480269a057d
JordanR21 <jjr8020@gmail.com>
2024-10-07 10:32:35 -0500

    test commit

--- src/catalog.js
+++ src/catalog.js
@@ -3,7 +3,7 @@ const products = [
     id:1,
     name:"Product Example",
     price: 19.99,
-    images: [],
+    images: ["src/assets/imgs/blank_candle.jpg"],
     stock: 100,
     description: "This product is totally not a scam."
 }
