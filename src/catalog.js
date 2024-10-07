ab8652c7bf41a83e9c3903307ecf4327380c223d
Ha-Ven <haven.mcdougle@my.utsa.edu>
2024-10-07 11:02:18 -0500

    updated products for catalog

--- src/catalog.js
+++ src/catalog.js
@@ -1,11 +1,169 @@
 const products = [
-{
-    id:1,
-    name:"Product Example",
-    price: 19.99,
-    images: [],
-    stock: 100,
-    description: "This product is totally not a scam."
-}
+    {
 
+        //----------All Year Round Scents-----------
+        id: 1,
+        name: "The Magic",
+        price: 19.99,
+        images: [],
+        stock: 100,
+        description: "This product is totally not a scam."
+    },
+    {
+        id: 2,
+        name: "Rose",
+        price: 19.99,
+        images: [],
+        stock: 80,
+        description: "You can't go wrong with the original."
+
+    },
+    {
+        id: 3,
+        name: "Vinilla Bean", 
+        price: 19.99,
+        images: [],
+        stock: 80,
+        description: "Nothing's better than having the smell of pumpkins in the air."
+    },
+    {
+        id: 4,
+        name: "Maple Syrup",
+        price: 19.99,
+        images: [],
+        stock: 80,
+        description: "This product is totally not a scam."
+    },
+    {
+        id: 5,
+        name: "Rain Rain Rain",
+        price: 19.99,
+        images: [],
+        stock: 80,
+        description: "This product is totally not a scam."
+    
+    },
+    {
+        id: 6,
+        name: "Tu Casa",
+        price: 19.99,
+        images: [],
+        stock: 80,
+        description: "This product is totally not a scam."
+    },
+    {
+        id: 7,
+        name: "Beach Day",
+        price: 19.99,
+        images: [],
+        stock: 80,
+        description: "This product is totally not a scam."
+    },
+    {
+        id: 8,
+        name: "Ocean Water",
+        price: 19.99,
+        images: [],
+        stock: 80,
+        description: "This product is totally not a scam."
+    },
+    {
+        id: 9,
+        name: "Cherry Blossom",
+        price: 19.99,
+        images: [],
+        stock: 80,
+        description: "This product is totally not a scam."
+    },
+    {
+        id: 10,
+        name: "Apple Pie",
+        price: 19.99,
+        images: [],
+        stock: 80,
+        description: "This product is totally not a scam."
+    },
+    {
+        id: 11,
+        name: "Forrest Pine",
+        price: 19.99,
+        images: [],
+        stock: 80,
+        description: "This product is totally not a scam."
+    },
+    {
+        id: 12,
+        name: "Cafe en Paris",
+        price: 19.99,
+        images: [],
+        stock: 80,
+        description: "This product is totally not a scam."
+    },
+    //--------------Seasonal Scents (Autumn-----------)
+    {
+        id: 13,
+        name: "Autumn Breeze",
+        price: 24.99,
+        images: [],
+        stock: 50,
+        description: "This product is totally not a scam."
+    },
+    {
+        id: 14,
+        name: "Pumknin Spice Latte",
+        price: 24.99,
+        images: [],
+        stock: 50,
+        description: "This product is totally not a scam."
+    },
+    {
+        id: 15,
+        name: "Pumpkin Patch",
+        price: 24.99,
+        images: [],
+        stock: 50,
+        description: "This product is totally not a scam."
+        
+    },
+    {
+         id: 16,
+         name: "Haunted",
+         price: 24.99,
+         images: [],
+         stock: 50,
+         description: "This product is totally not a scam."
+    },
+    //--------------------Seasonal Scents( Christmas-----------)
+    {
+        id: 17,
+        name: "Sugar Cookies",
+        price: 24.99,
+        images: [],
+        stock: 75,
+        description: "This product is totally not a scam."
+    },
+    {
+        id: 18,
+        name: "The Perfect Present",
+        price: 24.99,
+        images: [],
+        stock: 75,
+        description: "This product is totally not a scam."
+    },
+    {
+        id: 19,
+        name: "Melted Snowman",
+        price: 24.99,
+        images: [],
+        stock: 75,
+        description: "This product is totally not a scam."
+    },
+    {
+        id: 20,
+        name: "GingerBread",
+        price: 24.99,
+        images: [],
+        stock: 75,
+        description: "This product is totally not a scam."
+    },
 ]
